5d688faaa6e7869bccb9b72eea1d654ec1fc4968
Andre Loewen <Andre010400@web.de>
2026-01-01 17:52:11 +0100

    Fix misalignment issues with big card titles

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const CARD_WIDTH = 40;
+const ICON_WIDTH = 40;
 
 type CardProps = {
   name: string;
@@ -10,23 +10,23 @@ type CardProps = {
 
 function getIconURL(url: string): string {
   const domain = new URL(url).hostname;
-  return `https://www.google.com/s2/favicons?domain=${domain}&sz=${CARD_WIDTH}`;
+  return `https://www.google.com/s2/favicons?domain=${domain}&sz=${ICON_WIDTH}`;
 }
 
 export default function Card({ name, url, iconLink }: CardProps) {
   return (
     <a href={url}>
-      <div className="p-1 flex flex-col justify-center items-center">
+      <div className="p-1 flex flex-col justify-center items-center w-20">
         <div className="bg-slate-700 shadow shadow-black w-15 h-15 rounded-xl flex items-center justify-center mb-2">
           <Image
             alt="Logo"
             src={getIconURL(iconLink ?? url)}
-            width={CARD_WIDTH}
-            height={CARD_WIDTH}
+            width={ICON_WIDTH}
+            height={ICON_WIDTH}
             className="flex-none"
           />
         </div>
-        <h4>{name}</h4>
+        <h4 className="text-ellipsis overflow-hidden max-w-30">{name}</h4>
       </div>
     </a>
   );
